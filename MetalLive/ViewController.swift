24f48cf44df2ea187a324924d3e858460191aa8b
Gustavo Branco <gbranco@netflix.com>
2021-03-10 19:56:05 -0800

    Initial Commit

---
+++ MetalLive/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalLive
+//
+//  Created by Gustavo Branco on 3/10/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
